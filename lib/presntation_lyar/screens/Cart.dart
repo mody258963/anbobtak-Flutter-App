6bf36dd14fc757579dfdb5775f1020eb015dd136
mody258963 <122531523+mody258963@users.noreply.github.com>
2024-09-17 05:22:13 +0300

    last

--- lib/presntation_lyar/screens/Cart.dart
+++ lib/presntation_lyar/screens/Cart.dart
@@ -1,3 +1,4 @@
+import 'package:anbobtak/costanse/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -12,8 +13,16 @@ class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      Sc
-
+      home: Scaffold(
+        backgroundColor: MyColors.white,appBar: AppBar(
+        title: Text('Cart'),
+             leading: IconButton(
+          icon: Icon(Icons.arrow_back),  // Back arrow icon
+          onPressed: () {
+            Navigator.pop(context);  // Navigate back when pressed
+          },
+        ),
+      ),),
     );
   }
 }
